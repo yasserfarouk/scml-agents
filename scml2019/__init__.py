7d6f15cd21aba6ff63e6c1c1c481da0a7b7f623d
Yasser Mohammad <yasserfarouk@gmail.com>
2020-07-31 11:36:42 +0900

    adding scml2019 agents

---
+++ scml2019/__init__.py
@@ -0,0 +1,15 @@
+from .fj2 import FJ2FactoryManager
+from .rapt_fm import RaptFactoryManager
+from .iffm import InsuranceFraudFactoryManager
+from .saha import SAHAFactoryManager
+from .cheap_buyer.cheapbuyer import CheapBuyerFactoryManager
+from .nvm.nmv_agent import NVMFactoryManager
+
+__all__ = [
+    "FJ2FactoryManager",
+    "RaptFactoryManager",
+    "InsuranceFraudFactoryManager",
+    "SAHAFactoryManager",
+    "CheapBuyerFactoryManager",
+    "NVMFactoryManager",
+]
