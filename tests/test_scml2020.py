5cbf18b2e652eb9116a895c6275f8f5289b29b03
Yasser Mohammad <yasserfarouk@gmail.com>
2021-07-08 11:22:32 +0900

    Testing MontyHall with 50 steps
    MontyHall by construction works only for some step lengths including
    anything between 50 and 100.

--- tests/test_scml2020.py
+++ tests/test_scml2020.py
@@ -16,11 +16,12 @@ import hypothesis.strategies as st
 from scml.scml2020 import SCML2020World
 from scml_agents.scml2020 import *
 from scml_agents import get_agents
+from scml_agents.scml2020.monty_hall import MontyHall
 
 
 @mark.parametrize("fm", get_agents(2020, as_class=True))
 def test_can_run(fm):
-    n_steps = 5
+    n_steps = 5 if fm != MontyHall else 50
     world = SCML2020World(
         **SCML2020World.generate(
             agent_types=["scml.scml2020.DecentralizingAgent", fm], n_steps=n_steps,
